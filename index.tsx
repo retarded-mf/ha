772d48b8f1020914b4bec4e3cabbf08a2c6c91ae
Jay Gandhi <gdchamp38@gmail.com>
2025-09-21 05:39:43 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -6,10 +6,5 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-
 const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
